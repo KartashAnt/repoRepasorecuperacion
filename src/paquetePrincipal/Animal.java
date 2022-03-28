cfc7bfe7f0b47bfbe24b0716c253a488277d286e
User <User@DESKTOP-LG7RT9N>
2022-03-28 13:13:06 +0200

    Version 7.1 - inicia refactorizacion (clase Animal refactorizado)

--- src/paquetePrincipal/Animal.java
+++ src/paquetePrincipal/Animal.java
@@ -1,51 +1,81 @@
 package paquetePrincipal;
 
 public class Animal {
+	
 	private String nombre;
 	private double peso;
 	private int edad;
 	private boolean muerto = false;
-
+	
+	//Constructor con todos los datos del animal
 	public Animal(String nombre, double peso, int edad) {
+		
 		super();
+		
 		this.nombre = nombre;
 		this.peso = peso;
 		this.edad = edad;
+		
 	}
-
+	
+	//Constructor vacío
 	public Animal() {
+		
 		super();
+		
 	}
-
+	
+	//toString
 	@Override
 	public String toString() {
+		
 		return nombre + ": peso: " + peso + " Kg, edad: " + edad + " anyos";
+		
 	}
 
+	//El metodo que sobrescribo en los hijos para tener acceso al toString de padre
 	public String superToString() {
+		
 		return "";
+		
 	}
 
+	//Metodo para añadir un año al animal y demostrar su edad actualizada
 	public int sumarAnyo() {
+		
 		this.edad++;
 		return this.edad;
+		
 	}
 
+	//Metodo pa cambiar el peso de un animal
 	public void cambiarPeso(double peso) {
+		
 		String cambio = "";
+		
+		//Si el animal gana o pierde el peso
 		if (peso > this.peso)
 			cambio = "ganado";
 		else
 			cambio = "perdido";
+		
+		//Demuestro cuando peso ha ganado/perdido un tal animal
 		System.out.println(this.nombre + " ha " + cambio + " " + Math.abs(peso - this.peso));
+		
+		//Asigno el valor nuevo
 		this.peso = peso;
+		
 	}
-
+	
+	//Marco ese animal como muerto
 	public void marcarMuerto() {
+		
 		System.out.println("El animal " + this.nombre + " ha muerto");
 		this.muerto = true;
+		
 	}
-
+	
+	//setters y getters
 	public String getNombre() {
 		return nombre;
 	}
